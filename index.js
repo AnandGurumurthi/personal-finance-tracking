0eba6061dd888c90837ba44d55341bad3404bb38
Anand Gurumurthi <agurumurthi@agurumurth-ltm3.internal.salesforce.com>
2016-12-21 13:43:41 -0800

    add form

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+var fs = require('fs');
 var app = express();
+var formidable = require("formidable");
+var util = require('util');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -29,3 +32,40 @@ app.get('/times', function(request, response) {
       result += i + ' ';
   response.send(result);
 });
+
+app.get('/test', function(request, response) {
+	displayForm(response);
+});
+
+app.post('/test', function(request, response) {
+	processAllFieldsOfTheForm(request, response);
+});
+
+function displayForm(response) {
+    //response.render('pages/forms');
+    fs.readFile('form.html', function (err, data) {
+        response.writeHead(200, {
+            'Content-Type': 'text/html',
+                'Content-Length': data.length
+        });
+        response.write(data);
+        response.end();
+    });
+}
+
+function processAllFieldsOfTheForm(request, response) {
+    var form = new formidable.IncomingForm();
+
+    form.parse(request, function (err, fields, files) {
+        //Store the data from the fields in your data store.
+        //The data store could be a file or database or any other store based
+        //on your application.
+        response.writeHead(200, {
+            'content-type': 'text/plain'
+        });
+        response.write('received the data:\n\n');
+        response.end(util.inspect({
+            fields: fields
+        }));
+    });
+}
