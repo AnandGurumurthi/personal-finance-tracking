f9ef7f6ad46fb7af9d039ee17ac7bd9338f1ecca
Anand Gurumurthi <agurumurthi@agurumurth-ltm3.internal.salesforce.com>
2016-12-26 13:22:00 -0800

    Authentication and moving to handlebars

--- index.js
+++ index.js
@@ -1,98 +1,94 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
-var fs = require('fs');
-var app = express();
-var formidable = require("formidable");
-var mongodb = require('mongodb');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://test:test@ds141108.mlab.com:41108/heroku_w4v89h8f');
+var db = mongoose.connection;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var finance = require('./routes/finance');
+
+helpers = require('./helpers/handlebar-helpers');
+
+// Init App
+var app = express();
+
+// View Engine
+var hbs = exphbs.create({
+    defaultLayout: 'layout',
+    helpers      : helpers
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+// Passport init
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// Connect Flash
+app.use(flash());
+
+// Global Vars
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/finance', finance);
+
+// Set Port
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index')
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-app.get('/addExpense', function(request, response) {
-  response.render('pages/expense');
-});
-
-app.post('/addExpense', function(request, response) {
-  processAllFieldsOfTheForm(request, response, "expense");
-});
-
-app.get('/addIncome', function(request, response) {
-  response.render('pages/income');
-});
-
-app.post('/addIncome', function(request, response) {
-  processAllFieldsOfTheForm(request, response, "income");
-});
-
-function processAllFieldsOfTheForm(request, response, type) {
-    var form = new formidable.IncomingForm();
-    form.parse(request, function (err, fields, files) {
-        insertData(fields, response, type);
-    });
-}
-
-function insertData(fields, response, type) {
-  console.log(fields);
-  var status = true;
-  var MongoClient = mongodb.MongoClient;
-  var url = 'mongodb://test:test@ds141108.mlab.com:41108/heroku_w4v89h8f';
-  MongoClient.connect(url, function (err, db) {
-      if (err) {
-        response.send('Unable to connect to the Server', err);
-    } else {
-        var collection = db.collection('finance');
-        collection.insert(fields, function(err, result) {
-            if (err) {
-                status = false;
-            }
-        //Close connection
-        db.close();
-    });
-    }
-  });
-  if(type == 'income') {
-    response.render('pages/income', {
-        result: status
-    });
-  } else {
-    response.render('pages/expense', {
-        result: status
-    });
-  }
-}
-
-app.get('/list', function(request, response){
-  var MongoClient = mongodb.MongoClient;
-  var url = 'mongodb://test:test@ds141108.mlab.com:41108/heroku_w4v89h8f';
-  MongoClient.connect(url, function (err, db) {
-      if (err) {
-        response.send('Unable to connect to the Server', err);
-    } else {
-        var collection = db.collection('finance');
-        collection.find({}).toArray(function (err, result) {
-          if (err) {
-            response.send(err);
-        } else {
-            response.render('pages/list', {
-                results: result
-            });
-        } 
-        //Close connection
-        db.close();
-      });
-    }
-  });
+app.listen(app.get('port'), function(){
+    console.log('Server started on port '+app.get('port'));
 });
---
+++ routes/finance.js
@@ -0,0 +1,117 @@
+var express = require('express');
+var router = express.Router();
+
+var Finance = require('../models/finance');
+
+// Income
+router.get('/income', ensureAuthenticated, function(req, res){
+	res.render('finance/income', {
+		title: 'Income - '
+	});
+});
+
+router.post('/income', ensureAuthenticated, function(req, res){
+	var username = req.user.username;
+	var category = req.body.category;
+	var amount = req.body.amount;
+	var type = 'Income';
+
+	// Validation
+	req.checkBody('category', 'Category is required').notEmpty();
+	req.checkBody('amount', 'Amount is required').notEmpty();
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('finance/income',{
+			title: 'Income - ',
+			errors: errors
+		});
+	} else {
+		var newTransaction = new Finance({
+			username: username,
+			amount: amount,
+			category: category,
+			type: type
+		});
+
+		Finance.createTransaction(newTransaction, function(err, user){
+			if(err) throw err;
+			console.log(newTransaction);
+		});
+
+		req.flash('success_msg', 'Income successfully created');
+		res.redirect('/finance/income');
+	}
+});
+
+// Expense
+router.get('/expense', ensureAuthenticated, function(req, res){
+	res.render('finance/expense', {
+		title: 'Expense - '
+	});
+});
+
+router.post('/expense', ensureAuthenticated, function(req, res){
+	var username = req.user.username;
+	var category = req.body.category;
+	var amount = req.body.amount;
+	var type = 'Expense';
+
+	// Validation
+	req.checkBody('category', 'Category is required').notEmpty();
+	req.checkBody('amount', 'Amount is required').notEmpty();
+
+	var errors = req.validationErrors();
+
+	if(errors){
+		res.render('finance/expense',{
+			title: 'Expense - ',
+			errors: errors
+		});
+	} else {
+		var newTransaction = new Finance({
+			username: username,
+			amount: amount,
+			category: category,
+			type: type
+		});
+
+		Finance.createTransaction(newTransaction, function(err, transaction){
+			if(err) throw err;
+			console.log(transaction);
+		});
+
+		req.flash('success_msg', 'Expense successfully created');
+		res.redirect('/finance/expense');
+	}
+});
+
+// View All
+router.get('/view-all', ensureAuthenticated, function(req, res){
+	Finance.getAllTransaction(req.user.username, function(err, results){
+		if(err) throw err;
+		res.render('finance/view-all',{
+			title: 'View All - ',
+			results: results
+		});
+	});
+});
+
+function ensureAuthenticated(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	} else {
+		req.flash('error_msg','You are not logged in');
+		res.redirect('/users/login');
+	}
+}
+
+function processAllFieldsOfTheForm(request, response, type) {
+	var form = new formidable.IncomingForm();
+	form.parse(request, function (err, fields, files) {
+		insertData(fields, response, type);
+	});
+}
+
+module.exports = router;
