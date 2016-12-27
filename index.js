6ca1ca2e9409a8b100445227b4d133e23c8ba338
Anand Gurumurthi <agurumurthi@agurumurth-ltm3.internal.salesforce.com>
2016-12-27 11:57:31 -0800

    Moved config to env property

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://test:test@ds141108.mlab.com:41108/heroku_w4v89h8f');
+mongoose.connect(process.env.MONGO_URL);
 var db = mongoose.connection;
 
 var routes = require('./routes/index');
